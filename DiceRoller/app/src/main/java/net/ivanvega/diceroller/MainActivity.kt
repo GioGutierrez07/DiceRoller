6301c46dddeaeca4eaa26bc7cec7f49a23b878bb
giova@LAPTOP-SS13RSNT <s20120005@alumnos.itsur.edu.mx>
2023-09-06 21:12:45 -0600

    practica finalizada

--- DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -1,22 +1,30 @@
 package net.ivanvega.diceroller
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.ivanvega.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,17 +62,43 @@ val l = @Composable(){
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember{ mutableStateOf(5) }
+    var result2 by remember{ mutableStateOf(5) }
 
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
-        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = stringResource(
-            R.string.image_cara_1
-        ) )
-        Button(onClick = { /*TODO*/ }) {
+        getRandomDiceImage(result)
+        getRandomDiceImage(result2)
+
+        Spacer(modifier = Modifier.height(16.dp));
+        Button(onClick = { result = (1..6).random()
+              result2 = (1..6).random()
+        })
+        //  Log.d("NumAlea", "Valor aleatorio")
+        {
             Text(text = stringResource(R.string.btn_roller))
         }
     }
+    }
+    @Composable
+    fun getRandomDiceImage(imagen:Int) {
+        val imagesResource2 = when(imagen){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
 
-}
+        }
+        Image(painter = painterResource(id = imagesResource2), contentDescription = stringResource(
+            R.string.image_cara_2
+        ) )
+    }
+
+
+
+
+
 
 
 /*
